33e0c20d39ad3b5c6f8f1d4d63d0fcd6f60abcea
wsn <123@qq.com>
2023-05-10 20:54:26 +0800

    todos commit

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
           
-          <todo v-for="item in list" :key="item.id" :item="item" @del="del"></todo>
+          <todo v-for="item in arr" :key="item.id" :item="item" @del="del"></todo>
 
       </ul>
     </section>
@@ -26,13 +26,13 @@
       <span class="todo-count">剩余<strong>{{nodo}}</strong>未完成 </span>
       <ul class="filters">
         <li>
-          <a class="selected" href="#/">全部</a>
+          <a :class="{selected: num == 1}" href="#/" @click.prevent="change(1)">全部</a>
         </li>
         <li>
-          <a href="#/active">未完成</a>
+          <a :class="{selected: num == 2}" href="#/active" @click.prevent="change(2)">未完成</a>
         </li>
         <li>
-          <a href="#/completed">已完成</a>
+          <a :class="{selected: num == 3}" href="#/completed" @click.prevent="change(3)">已完成</a>
         </li>
       </ul>
       <button class="clear-completed" @click="clearDone">清除已完成</button>
@@ -49,7 +49,9 @@ export default {
   data () {
     return {
       name: "",
-      list: JSON.parse(localStorage.getItem("todos")) || []
+      list: JSON.parse(localStorage.getItem("todos")) || [],
+      arr: JSON.parse(localStorage.getItem("todos")) || [],
+      num: 1,
     }
   },
   components:{
@@ -68,19 +70,22 @@ export default {
       this.name=""
     },
     clearDone(){
-      this.list.forEach(x=>{
-        if(x.isDone) {
-          let i = this.list.findIndex(e=>e.id == x.id)
-          this.list.splice(i,1)
-        }
-      })
+      this.list = this.list.filter(e=>e.isDone==false)
+    },
+    change(i){
+      this.num = i
+      if(1==i){
+        this.arr = this.list
+      } else if(2 == i) {
+        this.arr = this.list.filter(x=>x.isDone==false)
+      } else if (3 == i) {
+        this.arr = this.list.filter(x=>x.isDone==true)
+      }
     }
   },
   computed:{
     nodo(){
-      let num = 0
-      this.list.forEach(x=> x.isDone ? num+=0 : num+=1)
-      return num
+      return this.list.filter(e=>e.isDone==false).length
     },
     isAll:{
       set(b){
@@ -95,6 +100,7 @@ export default {
     list:{
       deep: true,
       handler(){
+        this.arr = this.list
         localStorage.setItem("todos",JSON.stringify(this.list))
       }
     }
