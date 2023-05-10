0cd059fdd4c6fe95141d487ccbe9664b0ca79317
wsn <123@qq.com>
2023-05-10 22:01:42 +0800

    父子组件通信 v-model 语法糖

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-    <cpt-1></cpt-1>
+    <!-- <cpt-1></cpt-1> -->
+    <cpt-2 :value="num" @input="fn"></cpt-2>
+    <cpt-3 v-model="num2"></cpt-3>
   </div>
 </template>
 
 <script>
 import cpt1 from "./components/01-v-model.vue"
+import cpt2 from "./components/02-cpt.vue"
+import cpt3 from "./components/03-cpt.vue"
 export default {
+  data(){
+    return{
+      num: 100,
+      num2: 100,
+    }
+  },
   components:{
-    cpt1
+    cpt1,
+    cpt2,
+    cpt3,
+  },
+  methods:{
+    fn(a){
+      this.num += a
+    }
   }
 }
 </script>
---
+++ src/components/03-cpt.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>库存: {{ value }}</p>
+    <button @click="sale">sale</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["value"],
+  methods: {
+    sale() {
+      this.$emit("input", -7);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
