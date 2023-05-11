dd14c03190e054d2cf73be0034324361553e8ceb
wsn <123@qq.com>
2023-05-11 11:07:18 +0800

    nextTick

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
     <!-- <cpt-2 :value="num" @input="fn"></cpt-2>
     <cpt-3 v-model="num2"></cpt-3> -->
     <!-- <cpt-4/> -->
-    <cpt-5 ref="aaa"/>
-    <button @click="get">获取</button>
+    <!-- <cpt-5 ref="aaa"/> -->
+    <!-- <button @click="get">获取</button> -->
+    <cpt-6/>
   </div>
 </template>
 
@@ -15,6 +16,7 @@ import cpt2 from "./components/02-cpt.vue"
 import cpt3 from "./components/03-cpt.vue"
 import cpt4 from "./components/04-cpt.vue"
 import cpt5 from "./components/05-cpt.vue"
+import cpt6 from "./components/06-cpt.vue"
 export default {
   data(){
     return{
@@ -28,14 +30,11 @@ export default {
     cpt3,
     cpt4,
     cpt5,
+    cpt6,
   },
   methods:{
     fn(a){
       this.num += a
-    },
-    get(){
-      console.log(this.$refs.aaa.info)
-      this.$refs.aaa.fn()
     }
   }
 }
---
+++ src/components/06-cpt.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p ref="p">{{num}}</p>
+    <button @click="fn">+1</button>
+    <hr/>
+    <button v-if="show" @click="fn2">点击</button>
+    <input v-else type="text" ref="p" />
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            num:1,
+            show: true
+        }
+    },
+    methods:{
+      fn(){
+        this.num++
+        console.log(this.$refs.p.innerHTML)
+        this.$nextTick(()=>{
+          console.log("===>"+this.$refs.p.innerHTML)
+        })
+      },
+      fn2(){
+        this.show = false
+        console.log(this.$refs.p)
+
+        this.$nextTick(()=>{
+          this.$refs.p.focus()
+        })
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
