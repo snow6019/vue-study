d798f5e7c82f3ba02a861f33b240473ec6a4fe25
wsn <123@qq.com>
2023-05-12 09:35:26 +0800

    day03

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,10 @@
 <template>
-  <div>
-    <!-- <cpt-1></cpt-1> -->
-    <!-- <cpt-2 :value="num" @input="fn"></cpt-2>
-    <cpt-3 v-model="num2"></cpt-3> -->
-    <!-- <cpt-4/> -->
-    <!-- <cpt-5 ref="aaa"/> -->
-    <!-- <button @click="get">获取</button> -->
-    <!-- <cpt-6/> -->
-    <!-- <cpt-7/> -->
-    <!-- <cpt-7dynamic/> -->
-    <!-- <h3 v-red>测试</h3>
-    <cpt-8/>
-    <h3 v-blue>测试222</h3>
-    <h3 v-color="'pink'">测试223</h3> -->
-    <hr/>
-    <!-- <h4 v-dance="time">测试1：疯狂跳舞</h4>
-    <button @click="time=50000">慢下来</button>
-    <button @click="time=50">快起来</button> -->
-    <cpt-9>
-      <ul>
-        <li>xxxxxxxxxxxxxx</li>
-        <li>xxxxxxxxxxxxxx</li>
-        <li>xxxxxxxxxxxxxx</li>
-        <li>xxxxxxxxxxxxxx</li>
-      </ul>
-    </cpt-9>
-    <cpt-9>
-      <img src="./assets/logo.png" alt="dfasfdsa">
-    </cpt-9>
-  </div>
+  
 </template>
 
 <script>
-import cpt1 from "./components/01-v-model.vue"
-import cpt2 from "./components/02-cpt.vue"
-import cpt3 from "./components/03-cpt.vue"
-import cpt4 from "./components/04-cpt.vue"
-import cpt5 from "./components/05-cpt.vue"
-import cpt6 from "./components/06-cpt.vue"
-import cpt7 from "./components/07-cpt.vue"
-import cpt7dynamic from "./components/07-cpt-dynamic.vue"
-import cpt8 from "./components/08-cpt.vue"
-import cpt9 from "./components/09-cpt.vue"
 export default {
-  data(){
-    return{
-      num: 100,
-      num2: 100,
-      time: 500
-    }
-  },
-  components:{
-    cpt1,
-    cpt2,
-    cpt3,
-    cpt4,
-    cpt5,
-    cpt6,
-    cpt7,
-    cpt7dynamic,
-    cpt8,
-    cpt9,
-  },
-  methods:{
-    fn(a){
-      this.num += a
-    }
-  }
+
 }
 </script>
 
