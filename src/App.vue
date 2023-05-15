fa23474516a262a329e80fc1c87c118ae5d2dbb4
wsn <123@qq.com>
2023-05-15 17:28:49 +0800

    vue生命周期-1阶段学习

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <cpt-1></cpt-1>
+  </div>
 </template>
 
 <script>
+import cpt1 from "./components/01-生命周期-1阶段.vue"
 export default {
-
+  components:{
+    cpt1
+  }
 }
 </script>
 
---
+++ src/components/01-生命周期-1阶段.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "测试数据",
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate ", this.msg);
+  },
+  created() {
+    console.log("created ", this.msg);
+  },
+};
+</script>
+
+<style>
+</style>
