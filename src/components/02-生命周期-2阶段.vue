024fed6fece4bb0810f7b9b6ace3eafa1c95a430
wsn <123@qq.com>
2023-05-15 17:37:01 +0800

    vue生命周期-2阶段

---
+++ src/components/02-生命周期-2阶段.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p id="box">我是测试数据</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "测试数据",
+    };
+  },
+  beforeMount() {
+    console.log("beforeMount ", document.querySelector("#box"));
+    console.log("beforeMount ", this.msg);
+  },
+  mounted() {
+    console.log("mounted ", document.querySelector("#box"));
+    console.log("mounted ", this.msg);
+  },
+};
+</script>
+
+<style>
+</style>
