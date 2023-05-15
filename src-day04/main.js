60226e60ec5243c196698bc9cb78f93bac8f65a4
wsn <123@qq.com>
2023-05-15 17:20:17 +0800

    day04

--- src-day04/main.js
+++ src-day04/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive("color",function(el,binding){
+  el.style.backgroundColor = binding.value
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
