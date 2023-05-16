4d3db2dbd1628de27c559ea8ce8ee112ce61b425
wsn <123@qq.com>
2023-05-16 16:48:48 +0800

    day05

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <div>
-    <!-- <cpt-1></cpt-1> -->
-    <cpt-2></cpt-2>
-  </div>
+  
 </template>
 
 <script>
-import cpt1 from "./components/01-生命周期-1阶段.vue"
-import cpt2 from "./components/02-生命周期-2阶段.vue"
 export default {
-  components:{
-    cpt1,
-    cpt2,
-  }
+
 }
 </script>
 
